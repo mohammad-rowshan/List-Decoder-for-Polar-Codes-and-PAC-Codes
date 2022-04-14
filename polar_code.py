e25ceec4dd98517dee44a4d292df673eebe8ddac
Mohammad Rowshan <37102311+mohammad-rowshan@users.noreply.github.com>
2022-04-14 17:45:39 +1000

    Update polar_code.py

--- polar_code.py
+++ polar_code.py
@@ -1,19 +1,14 @@
-# Polar Code Class incldung Decoding Method ########################################
+# Polar Code Class incldung Decoding Method ###########################################
 #
 # Copyright (c) 2021, Mohammad Rowshan
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without modification, 
-# are permitted provided that the following conditions are met:
-#
-# 1. Redistributions of source code must retain the above copyright notice, 
-#   this list of conditions and the following disclaimer.
-# 2. Redistributions in binary form must reproduce the above copyright notice,
-#   this list of conditions and the following disclaimer in the documentation
-#   and/or other materials provided with the distribution.
-#
+# are permitted provided that:
+# the source code retains the above copyright notice, and te redistribtuion condition.
+# 
 # Freely distributed for educational and research purposes
-###################################################################################
+#######################################################################################
 
 import polar_coding_exceptions as pcexc
 import polar_coding_functions as pcfun
